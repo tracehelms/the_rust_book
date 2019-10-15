f78444f305778b17052b1f1212bff1a29cb1ace6
Trace Helms <tracehelms@gmail.com>
2019-10-14 18:49:03 -0600

    finish chapter 14.2

---
+++ chapter14_2/src/lib.rs
@@ -0,0 +1,26 @@
+//! # My Crate
+//!
+//! `chapter14_2` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = chapter14_2::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
